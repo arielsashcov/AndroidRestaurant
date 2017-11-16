896ddbf30a628411a0da3a9dc0a808d31cd0e778
arielsashcov <ariel.sashcov@gmail.com>
2017-11-15 21:02:59 -0500

    15-Nov-2017 5

--- app/src/main/java/cgodin/qc/ca/androidrestaurant/activities/LoginActivity.java
+++ app/src/main/java/cgodin/qc/ca/androidrestaurant/activities/LoginActivity.java
@@ -1,18 +1,20 @@
 package cgodin.qc.ca.androidrestaurant.activities;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -35,6 +37,8 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 import cgodin.qc.ca.androidrestaurant.R;
@@ -73,6 +77,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
 
+
+
         //progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         user = new User(); // exception: user object must be initialized before all to avoid null exceptions
         initViews();    //initialize the views
@@ -80,7 +86,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         initObjects();  //initialize the objects
 
         /* Assuming at this step all views and objects are initialized */
-
+        printHashKey();
 
     }
 
@@ -97,8 +103,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     /* DO NOT REMOVE! */
     /* PRINT HASHKEY FOR FACEBOOK */
-    /*public void printHashKey() {
-        String TAG = "MY HASH KEY";
+    public void printHashKey() {
+        String TAG = "MY HASH KEY"; //
         try {
             PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
             for (android.content.pm.Signature signature : info.signatures) {
@@ -112,7 +118,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         } catch (Exception e) {
             Log.e(TAG, "printHashKey()", e);
         }
-    }*/
+    }
 
     private void initViews() {
         currentView = findViewById(R.id.nestedScrollViewLogin);
