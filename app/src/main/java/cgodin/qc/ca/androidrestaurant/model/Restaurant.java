b04fb33cae3328ba027f599783fda06c54bd5fea
arielsashcov <ariel.sashcov@gmail.com>
2017-11-19 23:54:31 -0500

    Last commit

--- app/src/main/java/cgodin/qc/ca/androidrestaurant/model/Restaurant.java
+++ app/src/main/java/cgodin/qc/ca/androidrestaurant/model/Restaurant.java
@@ -1,10 +1,13 @@
 package cgodin.qc.ca.androidrestaurant.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Mohamed on 13/11/2017.
  */
 
-public class Restaurant {
+public class Restaurant implements Parcelable{
 
     private String formatted_address;
     private String name;
@@ -14,11 +17,15 @@ public class Restaurant {
     private String img_link;
     private String place_url;
     private String formatted_phone_number;
+    private String lat;
+    private String lng;
 
     public Restaurant() {
     }
 
-    public Restaurant(String formatted_address, String name, double rating, String place_id, String photo_reference, String img_link, String place_url, String formatted_phone_number) {
+
+
+    public Restaurant(String formatted_address, String name, double rating, String place_id, String photo_reference, String img_link, String place_url, String formatted_phone_number, String lat, String lng) {
         this.formatted_address = formatted_address;
         this.name = name;
         this.rating = rating;
@@ -27,7 +34,34 @@ public class Restaurant {
         this.img_link = img_link;
         this.place_url = place_url;
         this.formatted_phone_number = formatted_phone_number;
+        this.lat = lat;
+        this.lng = lng;
     }
+
+    protected Restaurant(Parcel in) {
+        formatted_address = in.readString();
+        name = in.readString();
+        rating = in.readDouble();
+        place_id = in.readString();
+        photo_reference = in.readString();
+        img_link = in.readString();
+        place_url = in.readString();
+        formatted_phone_number = in.readString();
+        lat = in.readString();
+        lng = in.readString();
+    }
+
+    public static final Creator<Restaurant> CREATOR = new Creator<Restaurant>() {
+        @Override
+        public Restaurant createFromParcel(Parcel in) {
+            return new Restaurant(in);
+        }
+
+        @Override
+        public Restaurant[] newArray(int size) {
+            return new Restaurant[size];
+        }
+    };
 
     public String getFormatted_address() {
         return formatted_address;
@@ -93,6 +127,22 @@ public class Restaurant {
         this.formatted_phone_number = formatted_phone_number;
     }
 
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
     @Override
     public String toString() {
         return "Restaurant{" +
@@ -104,6 +154,28 @@ public class Restaurant {
                 ", img_link='" + img_link + '\'' +
                 ", place_url='" + place_url + '\'' +
                 ", formatted_phone_number='" + formatted_phone_number + '\'' +
+                ", lat='" + lat + '\'' +
+                ", lng='" + lng + '\'' +
                 '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeString(formatted_address);
+        dest.writeString(name);
+        dest.writeDouble(rating);
+        dest.writeString(place_id);
+        dest.writeString(photo_reference);
+        dest.writeString(img_link);
+        dest.writeString(place_url);
+        dest.writeString(formatted_phone_number);
+        dest.writeString(lat);
+        dest.writeString(lng);
     }
 }
